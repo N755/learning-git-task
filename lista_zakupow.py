3b6b70c4cdabcbce3f0a2d44f4bd7d38d1ca40d7
Anastasiia Sirenko <nastya9701@ukr.net>
2023-02-21 18:09:29 +0200

    All lista_zakupow.py

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -5,3 +5,4 @@ my_dict = {
 
 for k in my_dict:
   print(f"Я йду до {k.upper()} і купую там: ", ', '.join([w.upper() for w in my_dict[k]]))
+print(f"Всього разом я купую: {sum(len(val) for val in my_dict.values())} продуктів")
